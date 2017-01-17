7465ee6ece6c2054ec3512444b65a02e9216c7bb
gc_chenqiu <gc_chenqiu@huoyunren.com>
2017-01-17 10:57:59 +0800

    初始化工程

---
+++ dubbo_pre/dubbo.src/src/main/resources/dubbo-2.5.0-sources/com/alibaba/dubbo/remoting/transport/MultiMessageHandler.java
@@ -0,0 +1,30 @@
+package com.alibaba.dubbo.remoting.transport;
+
+import java.util.List;
+
+import com.alibaba.dubbo.remoting.Channel;
+import com.alibaba.dubbo.remoting.ChannelHandler;
+import com.alibaba.dubbo.remoting.RemotingException;
+
+/**
+ * @author <a href="mailto:gang.lvg@alibaba-inc.com">kimi</a>
+ */
+public class MultiMessageHandler extends AbstractChannelHandlerDelegate {
+
+    public MultiMessageHandler(ChannelHandler handler) {
+        super(handler);
+    }
+
+    @SuppressWarnings("unchecked")
+	@Override
+    public void received(Channel channel, Object message) throws RemotingException {
+        if (message instanceof List) {
+            List<Object> list = (List<Object>)message;
+            for(Object obj : list) {
+                handler.received(channel, obj);
+            }
+        } else {
+            handler.received(channel, message);
+        }
+    }
+}
